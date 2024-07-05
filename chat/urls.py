4b2212ba14de9b9e14f708915bc9821a4df9dfec
Guy Tihon <roadnoble@hotmail.com>
2024-07-05 14:24:47 +0200

    Add user authentication, user model, and initial migrations
    This commit adds automatic user authentication and two pieces of middleware for managing user sessions. It includes the creation of the initial migration files for Django, and introduces the MyUser model. This change also provides initial setup for the user login and registration functionalities such as corresponding HTML templates and Python scripts. The CSS stylings for the project are also included.

--- chat/urls.py
+++ chat/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from .views import Index, signup, ArticleListView, ArticleDetailView, ArticleCreateView, CommentCreateView, \
-    add_comment
-
-app_name = 'chat'
+from .views import Index, signup, ArticleListView, ArticleDetailView, ArticleCreateView, CommentCreateView
 
 urlpatterns = [
     path('', Index, name='index'),
     path('signup/', signup, name='signup'),
-    path('article_list/', ArticleListView.as_view(), name='article_list'),
-    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('create/', ArticleCreateView.as_view(), name='article_create'),
-    path('article/<int:pk>/comment/create/', CommentCreateView.as_view(), name='comment_create'),
-    path('article/<int:pk>/add_comment/', add_comment, name='add_comment'),  # Ajoutez cette ligne
+    path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('articles/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('articles/<int:pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
 ]
