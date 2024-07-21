61d702575378e84aefc0167db540b74dc8fe607f
Guy Tihon <roadnoble@hotmail.com>
2024-07-22 01:16:23 +0200

    Add user authentication, user model, and initial migrations
    This commit adds automatic user authentication and two pieces of middleware for managing user sessions. It includes the creation of the initial migration files for Django, and introduces the MyUser model. This change also provides initial setup for the user login and registration functionalities such as corresponding HTML templates and Python scripts. The CSS stylings for the project are also included.

---
+++ accounts/Merde/views.py
@@ -0,0 +1,28 @@
+# accounts/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('chat:index')  # Rediriger vers une page d'accueil après inscription
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
